f06aeb7cd46c9584514411586111662e04cac2b4
Yashavanthba <97616847+Yashavanthba@users.noreply.github.com>
2022-01-13 18:59:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ for c in cycles:
 
 def test_bucketing_by_number_of_cycles():
   print("Counting batteries by usage cycles...\n");
-  counts = count_batteries_by_usage([100, 300, 500, 600, 900, 1000])
-  assert(counts["lowCount"] == 2)
-  assert(counts["mediumCount"] == 3)
-  assert(counts["highCount"] == 1)
+  counts = count_batteries_by_usage([100, 300,400, 500, 600, 900,920, 1000])
+  assert(counts["lowCount"] == 1)
+  assert(counts["mediumCount"] == 4)
+  assert(counts["highCount"] == 2)
   print("Done counting :")
 
 
